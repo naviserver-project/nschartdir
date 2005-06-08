8f9e35f7f0daf58d402bb1da738089425da7d42a
Vlad Seryakov <vlad@crystalballinc.com>
2005-06-08 20:03:38 +0000

    Change license and wording in README files

--- nschartdir.c
+++ nschartdir.c
@@ -1,20 +1,24 @@
-/*
- * Copyright (c) 2001 Vlad Seryakov and all the Contributers
- * All rights reserved.
-
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
-
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
-
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc., 675
- * Mass Ave, Cambridge, MA 02139, USA.
+/* 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis,WITHOUT WARRANTY OF ANY KIND,either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * Alternatively,the contents of this file may be used under the terms
+ * of the GNU General Public License(the "GPL"),in which case the
+ * provisions of GPL are applicable instead of those above.  If you wish
+ * to allow use of your version of this file only under the terms of the
+ * GPL and not to allow others to use your version of this file under the
+ * License,indicate your decision by deleting the provisions above and   
+ * replace them with the notice and other provisions required by the GPL.
+ * If you do not delete the provisions above,a recipient may use your
+ * version of this file under either the License or the GPL.
+ * 
  */
 
 /*
@@ -39,6 +43,7 @@
  *
  *     Vlad Seryakov vlad@crystalballinc.com
  */
+
 #define USE_TCL8X
 extern "C" {
 #include "ns.h"
