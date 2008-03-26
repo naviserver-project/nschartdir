7f8a7fc1223a021b39aeb3ea1d32e427a530e90a
Vlad Seryakov <vlad@crystalballinc.com>
2008-03-26 20:18:02 +0000

    Minor updates to compile with recent Tcl

--- nschartdir.c
+++ nschartdir.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1(the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
@@ -8,17 +8,17 @@
  * basis,WITHOUT WARRANTY OF ANY KIND,either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
- * 
+ *
  * Alternatively,the contents of this file may be used under the terms
  * of the GNU General Public License(the "GPL"),in which case the
  * provisions of GPL are applicable instead of those above.  If you wish
  * to allow use of your version of this file only under the terms of the
  * GPL and not to allow others to use your version of this file under the
- * License,indicate your decision by deleting the provisions above and   
+ * License,indicate your decision by deleting the provisions above and
  * replace them with the notice and other provisions required by the GPL.
  * If you do not delete the provisions above,a recipient may use your
  * version of this file under either the License or the GPL.
- * 
+ *
  */
 
 /*
@@ -45,12 +45,17 @@
  */
 
 #define USE_TCL8X
+
 extern "C" {
 #include "ns.h"
 }
+
 #include "chartdir.h"
-#define VERSION  "0.9.6"
-#define MAX_LAYERS 5
+
+#define _VERSION           "0.9.6"
+
+#define MAX_LAYERS         5
+
 enum ChartType { XYChartType, PieChartType };
 enum LayerType { LineType, BarType, AreaType, TrendType, PieType };
 
@@ -131,7 +136,7 @@ extern "C" {
     NS_EXPORT int Ns_ModuleInit(char *server, char *module) {
         char *path;
 
-         Ns_Log(Notice, "nschartdir module version %s server: %s", VERSION, server);
+         Ns_Log(Notice, "nschartdir module version %s server: %s", _VERSION, server);
 
          path = Ns_ConfigGetPath(server, module, NULL);
          Ns_ConfigGetInt(path, "idle_timeout", &chartIdleTimeout);
@@ -1107,7 +1112,7 @@ static int LayerCmd(Ns_Chart * chart, int objc, Tcl_Obj * CONST objv[], Tcl_Inte
     case cmdSetDataCombineMethod:{
             int method;
             char *name;
-            char *DataCombineMethod[] = { "Overlay", "Stack", "Depth", "Side", 0 };
+            char *DataCombineMethod[] = { (char*)"Overlay", (char*)"Stack", (char*)"Depth", (char*)"Side", 0 };
 
             if (objc < 6 || !(name = Tcl_GetStringFromObj(objv[5], 0))) {
                 Tcl_WrongNumArgs(interp, 4, objv, "#layer datacombinemethod");
@@ -1294,11 +1299,11 @@ static int gradientColor(Ns_Chart * chart, int objc, Tcl_Obj * CONST objv[], Tcl
     }
     // Check if it is name
     if (argc == 1 && (name = Tcl_GetStringFromObj(argv[0], 0)) &&
-        (!strcasecmp("goldGradient", name) && (array = (int *) goldGradient)) ||
-        (!strcasecmp("silverGradient", name) && (array = (int *) silverGradient)) ||
-        (!strcasecmp("redMetalGradient", name) && (array = (int *) redMetalGradient)) ||
-        (!strcasecmp("blueMetalGradient", name) && (array = (int *) blueMetalGradient)) ||
-        (!strcasecmp("greenMetalGradient", name) && (array = (int *) greenMetalGradient))) {
+        ((!strcasecmp("goldGradient", name) && (array = (int *) goldGradient)) ||
+         (!strcasecmp("silverGradient", name) && (array = (int *) silverGradient)) ||
+         (!strcasecmp("redMetalGradient", name) && (array = (int *) redMetalGradient)) ||
+         (!strcasecmp("blueMetalGradient", name) && (array = (int *) blueMetalGradient)) ||
+         (!strcasecmp("greenMetalGradient", name) && (array = (int *) greenMetalGradient)))) {
         Tcl_SetObjResult(interp, Tcl_NewIntObj(chart->chart->gradientColor(array, angle, scale, startx, starty)));
         return TCL_OK;
     } else {
@@ -1369,7 +1374,7 @@ static int PieCmd(Ns_Chart * chart, int objc, Tcl_Obj * CONST objv[], Tcl_Interp
             int argc, labelc;
             Tcl_Obj **argv, **labelv;
 
-            if (objc < 5 |
+            if (objc < 5 ||
                 Tcl_ListObjGetElements(interp, objv[4], &argc, &argv) != TCL_OK ||
                 (objc > 5 && Tcl_ListObjGetElements(interp, objv[5], &labelc, &labelv) != TCL_OK)) {
                 Tcl_WrongNumArgs(interp, 4, objv, "data ?labels?");
